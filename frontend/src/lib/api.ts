597fca4b9233d59f4959f5d5f1485d91a97a8bf2
Darakhshan-dev <darakhshanifraque@gmail.com>
2026-07-23 16:53:21 +0530

    Update API_BASE

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = `${import.meta.env.VITE_API_URL}/api`;
+export { API_BASE };
 
 export const fetchQuestions = () => axios.get(`${API_BASE}/questions`);
 export const saveQuestion = (question) => axios.post(`${API_BASE}/questions`, question);
