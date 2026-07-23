45051084bc1fdd47ba0534181872a7e60b6da2a5
Darakhshan-dev <darakhshanifraque@gmail.com>
2026-07-23 22:56:50 +0530

    updated files

--- frontend/src/components/QuestionList.tsx
+++ frontend/src/components/QuestionList.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { API_BASE } from '../services/api';
+import { API_BASE } from '../lib/api';
 
 const QuestionList = () => {
   const [questions, setQuestions] = useState([]);
